a7b84eb65304ea3e2d80dcfb657ac32bc7f99a63
影凌 <36478298+KageRyo@users.noreply.github.com>
2022-12-18 15:00:43 +0800

    將註解文字改為正體中文

--- bot.py
+++ bot.py
@@ -2,50 +2,50 @@ import discord
 import requests
 import json
 
-# 设置 Discord intents
+# 設置 Discord intents
 intents = discord.Intents().all()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
 channel_id = "channelID"
 
-# 建立一个字典，用来储存每个使用者的聊天纪录
+# 建立字典以接續對話
 chat_history = {}
 
 @client.event
 async def on_message(message):
-    # 取得使用者 ID 和聊天内容
+    # 取得使用者 ID 和聊天內容
     user_id = message.author.id
     chat_content = message.content
     # 如果訊息的格式符合 "AIclear"，則進行處理
     if chat_content == "AIclear":
-        # 将使用者的聊天纪录清除
+        # 將使用者的聊天紀錄清除
         chat_history[user_id] = []
-        # 将文本傳回 Discord 频道
+        # 將文本傳回 Discord 頻道
         channel=client.get_channel(channel_id)
         await message.channel.send("已清除聊天紀錄")
         return
     # 如果訊息的格式符合 "AI" + 內容 + "，則進行處理
     if chat_content.startswith('AI"'):
-        # 去除頭尾的双引號
+        # 去除頭尾的雙引號
         chat_content = chat_content.strip('"')
         # 將字符串分割成三個部分，取出第二個部分的內容
         chat_content = chat_content.split('"')[1]
     else:
         # 如果訊息的格式不符合，則跳過不處理
         return
-    # 如果使用者 ID 不在字典中，則新增一个记录
+    # 如果使用者 ID 不在字典中，則新增一個記錄
     if user_id not in chat_history:
         chat_history[user_id] = []
 
-    # 取得使用者的聊天纪录
+    # 取得使用者的聊天紀錄
     user_history = chat_history[user_id]
 
-    # 如果聊天次数大于 30，則清除除了前一次聊天纪录以外的记忆
+    # 如果聊天次數大於 30，則清除除了前一次聊天紀錄以外的記憶
     if len(user_history) > 30:
         user_history = user_history[-1:]
 
-    # 使用 OpenAI API 回复使用者的訊息
+    # 使用 OpenAI API 回覆使用者的訊息
     headers = {
         "Authorization": "Bearer openaiAPI"
     }
@@ -60,14 +60,14 @@ async def on_message(message):
         jsondata = response.json()
         print(response.json())
         response_text = jsondata['choices'][0]['text']
-        # 将回复的文本傳回 Discord 频道
+        # 將回覆的文本傳回 Discord 頻道
         channel=client.get_channel(channel_id)
         await message.channel.send(response_text)
     else:
-        # 处理错误
+        # 處理錯誤
         print(response.json())
         pass
 
-    # 将使用者的聊天纪录加入聊天纪录中
+    # 將使用者的聊天紀錄加入聊天紀錄中
     user_history.append(chat_content)
 client.run("token")
