e779d948caee38af71877da238d71f30f2668dae
jason920612 <30766214+jason920612@users.noreply.github.com>
2022-12-18 13:29:22 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import discord
 import requests
+import json
 
 # 设置 Discord intents
 intents = discord.Intents().all()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-channel_id = "962354694997540935"
+channel_id = "channel id"
 
 # 建立一个字典，用来储存每个使用者的聊天纪录
 chat_history = {}
@@ -14,8 +15,15 @@ chat_history = {}
 @client.event
 async def on_message(message):
     # 取得使用者 ID 和聊天内容
+
     user_id = message.author.id
+    # 只取 "AI" 後面的字串
     chat_content = message.content
+    # 如果聊天內容不是以 "AI" 開頭，則直接跳過
+    if not chat_content.startswith("AI"):
+        return
+    # 取得聊天內容，並去掉頭尾的双引號
+    chat_content = chat_content.split("\"")[1]
     # 如果使用者 ID 不在字典中，則新增一个记录
     if user_id not in chat_history:
         chat_history[user_id] = []
@@ -29,18 +37,19 @@ async def on_message(message):
 
     # 使用 OpenAI API 回复使用者的訊息
     headers = {
-        "Authorization": "Bearer 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"
+        "Authorization": "Bearer openapikey"
     }
     data = {
-    "model": "text-davinci-002",
+    "model": "text-davinci-003",
     "prompt": f"{' '.join(user_history)} {chat_content}",
     "max_tokens": 1024,
     "temperature": 0.5,
 }
     response = requests.post("https://api.openai.com/v1/completions", headers=headers, json=data)
     if response.status_code == 200:
+        jsondata = response.json()
         print(response.json())
-        response_text = response.json()['text']
+        response_text = jsondata['choices'][0]['text']
         # 将回复的文本傳回 Discord 频道
         channel=client.get_channel(channel_id)
         await message.channel.send(response_text)
@@ -51,4 +60,4 @@ async def on_message(message):
 
     # 将使用者的聊天纪录加入聊天纪录中
     user_history.append(chat_content)
-client.run("key")
+client.run("bot token")
